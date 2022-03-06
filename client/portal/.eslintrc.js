66fe85de22e51e80a2bb191a565a22f915ed4ea9
Daniel <daniilterawars@gmail.com>
2022-03-06 14:31:07 +0300

    feat: eslint a11y

--- client/portal/.eslintrc.js
+++ client/portal/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
         'plugin:react/recommended',
         'plugin:react-hooks/recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:jsx-a11y/recommended',
         'next/core-web-vitals',
     ],
     'parser': '@typescript-eslint/parser',
