97650753ce3f2626dd5ce8e0add9ee642e7d36e3
Daniel <daniilterawars@gmail.com>
2022-03-06 17:41:54 +0300

    feat: images

--- client/portal/components/pages/home/hero/index.tsx
+++ client/portal/components/pages/home/hero/index.tsx
@@ -1,21 +1,36 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Lazy } from 'swiper';
+import Image from 'next/image'
+
+import Hero1 from 'public/images/Hero1.jpg'
+import Hero2 from 'public/images/Hero2.jpg'
+import Hero3 from 'public/images/Hero6.jpg'
+import Hero4 from 'public/images/Hero5.jpg'
 
 const mockHoroData = [
     {
+        id: 1,
         title: 'title1',
         description: 'description1',
+        image: Hero1,
     },
     {
+        id: 2,
         title: 'title2',
         description: 'description2',
+        image: Hero2,
     },
     {
+        id: 3,
         title: 'title3',
         description: 'description3',
+        image: Hero3,
     },
     {
+        id: 4,
         title: 'title4',
         description: 'description4',
+        image: Hero4,
     },
 ]
 
@@ -23,13 +38,24 @@ export const HomeHero = () => {
     return (
         <Swiper
             className="relative h-screen bg-sky-500"
+            modules={[Lazy]}
+            lazy
             loop
         >
                 {
                     mockHoroData.map((slide) => (
                         <SwiperSlide>
+                            <Image 
+                                className="swiper-lazy"
+                                src={slide.image}
+                                layout="fill"
+                                objectFit="fill"
+                            />
                             <div className="container mx-auto">
-                                <div className="absolute top-1/4">
+                                <div 
+                                    key={slide.id}
+                                    className="absolute top-1/4"
+                                >
                                     <h2>{slide.title}</h2>
                                     <p>{slide.description}</p>
                                 </div>
