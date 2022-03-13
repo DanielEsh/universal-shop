58f48eba03f46f47eeb559d77d1f4b7292c86f0b
Daniel <daniilterawars@gmail.com>
2022-03-13 13:59:32 +0300

    feat(header): fixed

--- client/portal/components/layouts/header/index.tsx
+++ client/portal/components/layouts/header/index.tsx
@@ -1,11 +1,45 @@
+import { useState, useEffect } from 'react'
+import cn from 'classnames'
 import { MainHeader } from './MainHeader'
 import { MenuHeader } from './MenuHeader'
 
 export const Header = () => {
+    const [isFixed, setIsFixed] = useState<boolean>(false)
+
+    useEffect(() => {
+        document.addEventListener('scroll', onScroll)
+
+        return () => {
+            document.removeEventListener('scroll', onScroll)
+        }
+    }, [])
+
+    const onScroll = () => {
+        checkVisibility()
+    }
+
+    const checkVisibility = () => {
+        if (window.pageYOffset > 800) {
+            setIsFixed(true)
+        } else {
+            setIsFixed(false)
+        }
+    }
+
+    const classes = cn(
+        'top-0 left-0 w-full z-10',
+        {
+            ['absolute']: !isFixed,
+            ['fixed']: isFixed,
+        },
+    )
+
     return (
-        <header className="absolute top-0 left-0 w-full z-10">
+        <header className={classes}>
             <MainHeader />
-            <MenuHeader />
+            {
+                !isFixed && <MenuHeader />
+            }
         </header>
     )
 }
