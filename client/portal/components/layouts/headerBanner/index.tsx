0605ad2180a3d260a30487a6d4170215f3fc201d
Daniel <daniilterawars@gmail.com>
2022-03-13 20:58:26 +0300

    feat(headerBanner): button

--- client/portal/components/layouts/headerBanner/index.tsx
+++ client/portal/components/layouts/headerBanner/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Button } from '@/components/ui/Button'
 
 export const HeaderBanner = () => {
     return (
-        <div className="bg-primary-200">
-            <div className="container mx-auto">
-                Header Banner
+        <a 
+            className="block w-full h-[56px] bg-primary-200"
+            href="/test"
+        >
+            <div className="flex justify-center items-center h-full container mx-auto">
+                200 бонусов за покупки
+
+                <Button 
+                    className="ml-6"
+                    size="small"
+                    color="secondary"
+                >
+                    Посмотреть
+                </Button>
             </div>
-        </div>
+        </a>
     )
 }
