d0ef4637a2dd70a0afaa7054035031c89fd8e2b6
Daniel <daniilterawars@gmail.com>
2022-03-17 22:38:56 +0300

    feat(AdvantagesCard): hover

--- client/portal/components/blocks/home/AdvantagesNew/AdvantagesCard.tsx
+++ client/portal/components/blocks/home/AdvantagesNew/AdvantagesCard.tsx
@@ -8,16 +8,16 @@ type Props = {
 
 export const AdvantagesCard = ({ title, icon, description }: Props) => {
     return (
-        <div className="bg-primary-400 p-[60px] flex flex-col justify-center items-center">
-            <h2 className="text-lg mb-[34px]">
+        <div className="group dark:bg-dark-400 dark:text-white p-[60px] border-t-[1px] border-r-[1px] border-dark-300 flex flex-col justify-center items-center transition-all ease-in-out duration-300 hover:bg-primary-400 hover:text-black hover:scale-[1.1] hover:border-primary-400">
+            <h2 className="text-lg mb-[64px]">
                 {title}
             </h2>
 
-            <span className="w-[84px] h-[84px]">
+            <span className="w-[84px] h-[84px] transition-transform ease-in-out duration-300 group-hover:scale-150">
                 {icon}
             </span>
 
-            <p className="opacity-0">
+            <p className="mt-[60px] opacity-0 transition-opacity ease-in-out duration-300 group-hover:opacity-100">
                 {description}
             </p>
         </div>
