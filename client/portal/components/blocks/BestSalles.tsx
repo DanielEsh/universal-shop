a549c32bcfe1132a27cf3e728c4e0d8fc320a031
Daniel <daniilterawars@gmail.com>
2022-03-20 15:21:02 +0300

    feat(BestSalles): title and tabs

--- client/portal/components/blocks/BestSalles.tsx
+++ client/portal/components/blocks/BestSalles.tsx
@@ -3,57 +3,56 @@ import { ColoredTitle } from '@/ui/components/Typography/ColoredTitle'
 import { Tabs } from '@/ui/components/Tabs'
 
 export const BestSalles = () => {
-    const title = () => (
-        <ColoredTitle 
-            label="Хит"
-            colored="продаж"
-            className="mb-[24px]"
-        />
-    )
-
-    const tabs = () => (
-        <Tabs
-            defaultActiveTab={'1'}
-        >
-            <Tabs.Bar>
-                <Tabs.Tab value="0">
-                    Recent
-                </Tabs.Tab>
-                <Tabs.Tab value="1">
-                    Popular
-                </Tabs.Tab>
-                <Tabs.Tab
-                    value="2"
-                >
-                    Test
-                </Tabs.Tab>
-                <Tabs.Tab
-                    value="3"
-                >
-                    Trading
-                </Tabs.Tab>
-            </Tabs.Bar>
-            <div className="flex justify-center items-center max-w-md h-20 mt-4 bg-primary-500 rounded-md">
-                <Tabs.Panel value="0">
-                    Recent panel
-                </Tabs.Panel>
-                <Tabs.Panel value="1">
-                    Popular panel
-                </Tabs.Panel>
-                <Tabs.Panel value="2">
-                    Test panel
-                </Tabs.Panel>
-                <Tabs.Panel value="3">
-                    Trading panel
-                </Tabs.Panel>
-            </div>
-        </Tabs>
-    )
-
     return (
         <div className="py-[48px]">
-            <ProductShowcase
+            <ColoredTitle 
+                label="Хит"
+                colored="продаж"
+                className="mb-[24px]"
             />
+
+            <Tabs
+                defaultActiveTab={'1'}
+            >
+                <Tabs.Bar>
+                    <Tabs.Tab value="0">
+                        Ноутбуки
+                    </Tabs.Tab>
+                    <Tabs.Tab value="1">
+                        Планшеты
+                    </Tabs.Tab>
+                    <Tabs.Tab
+                        value="2"
+                    >
+                        Смартфоны
+                    </Tabs.Tab>
+                    <Tabs.Tab
+                        value="3"
+                    >
+                        Телевизоры
+                    </Tabs.Tab>
+                </Tabs.Bar>
+                
+                <Tabs.Panel value="0">
+                    <ProductShowcase
+                    />
+                </Tabs.Panel>
+                <Tabs.Panel value="1">
+                    <div className="text-white">
+                        Планшеты
+                    </div>
+                </Tabs.Panel>
+                <Tabs.Panel value="2">
+                    <div className="text-white">
+                        Смартфоны
+                    </div>
+                </Tabs.Panel>
+                <Tabs.Panel value="3">
+                    <div className="text-white">
+                        Телевизоры
+                    </div>
+                </Tabs.Panel>
+            </Tabs>
         </div>
     )
 }
