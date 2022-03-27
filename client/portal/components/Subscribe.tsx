d4f0bc0115c555b9d7fe9efec2e919c62c9f2415
Daniel <daniilterawars@gmail.com>
2022-03-27 14:56:57 +0300

    feat: Subscribe

--- client/portal/components/Subscribe.tsx
+++ client/portal/components/Subscribe.tsx
@@ -3,8 +3,8 @@ import { Input } from '@/ui/components/Input'
 
 export const Subscribe = () => {
     return (
-        <div className="w-[1200px] h-[154px] mx-auto bg-primary-500">
-            <div className="flex justify-between items-center h-full px-[148px]">
+        <div className="w-[1200px] h-[154px] mx-auto rounded-lg bg-primary-500">
+            <div className="flex justify-between items-center h-full px-[68px]">
                 <div className="flex">
                     <div>
                         <Input 
@@ -19,7 +19,11 @@ export const Subscribe = () => {
                     </div>
                 </div>
 
-                <Button color="secondary">
+                <Button 
+                    className="w-[176px]"
+                    color="secondary"
+                    size="large"
+                >
                     Подписаться
                 </Button>
             </div>
