78a0c6e37ca1d265737d780dae5f9efc28914d14
Daniel <daniilterawars@gmail.com>
2022-03-27 14:28:32 +0300

    feat: Partners

--- client/portal/components/blocks/home/Partners/index.tsx
+++ client/portal/components/blocks/home/Partners/index.tsx
@@ -1,17 +1,30 @@
 import { PartnerCard } from '@/components/cards/PartnerCard'
+import { ColoredTitle } from '@/components/ui/components/Typography/ColoredTitle'
 
 export const Partners = () => {
     return (
-        <div>
-            Partners
+        <div className="py-10">
+            <ColoredTitle 
+                className="mb-10"
+                label="Наши"
+                colored="партнеры"
+            />
 
-            <div className="grid gap-[20px] grid-cols-6">
+            <div className="grid grid-cols-5 border dark:border-dark-300">
                 <PartnerCard />
                 <PartnerCard />
                 <PartnerCard />
                 <PartnerCard />
                 <PartnerCard />
+            </div>
+            <div className="grid grid-cols-5 border border-t-0 dark:border-dark-300">
                 <PartnerCard />
+                <PartnerCard />
+                <PartnerCard />
+                <PartnerCard />
+                <div className="flex justify-center items-center h-[84px] p-[48px] dark:bg-dark-400 dark:text-white">
+                    Ещё 150+
+                </div>
             </div>
         </div>
     )
--- client/portal/components/cards/PartnerCard/index.tsx
+++ client/portal/components/cards/PartnerCard/index.tsx
@@ -3,8 +3,10 @@ import SamsungLogo from 'public/images/samsung.png'
 
 export const PartnerCard = () => {
     return (
-        <div className="flex justify-center items-center h-[84px] p-4 bg-gray-100 rounded-md">
+        <div className="flex justify-center items-center h-[84px] p-[48px] border-r last:border-none bg-gray-100 
+        dark:bg-dark-400 dark:text-white dark:border-dark-300">
             <Image 
+                className="backdrop-grayscale"
                 src={SamsungLogo}
                 alt=""
             />
